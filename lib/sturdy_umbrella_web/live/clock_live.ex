db7a02b6e0957e3683a86311e54b47b729ef0f5c
Marco Monaco <marco@viafoura.com>
2019-03-22 13:04:17 -0400

    list of 100 pages

--- lib/sturdy_umbrella_web/live/clock_live.ex
+++ lib/sturdy_umbrella_web/live/clock_live.ex
@@ -1,13 +1,22 @@
 defmodule SturdyUmbrellaWeb.ClockLive do
   use Phoenix.LiveView
 
+  require Logger
   # Must be a number greater than or equal to 1
   #   Erlang only supports resolutions of 1ms or greater.
   @update_frequency 1
 
   # Called by `live_render` in our template
   def render(assigns) do
-    ~L{Current time (UTC): <%= @time %>}
+    ~L[<ol><%= for {page, count} <- @time do %>
+       <li><%= page %> has <%= count %> views</li>
+       <% end %>
+       </ol>
+    ]
+  end
+
+  def count_changed(thing) do
+    send self(), {:update}
   end
 
   # Runs once, on page load
@@ -24,6 +33,6 @@ defmodule SturdyUmbrellaWeb.ClockLive do
   end
 
   defp update_time(socket) do
-    assign(socket, :time, Time.utc_now())
+    assign(socket, :time, Enum.take(Enum.sort(:ets.tab2list(:page_cache), fn ({page1, count1}, {page2, count2}) -> count1 > count2 end), 100))
   end
 end
--- lib/sturdy_umbrella_web/umbrella_consumer.ex
+++ lib/sturdy_umbrella_web/umbrella_consumer.ex
@@ -1,21 +1,24 @@
 defmodule SturdyUmbrellaWeb.KafkaConsumer do
 
   @name :page_cache
+  require Logger
 
   # function to accept Kafka messaged MUST be named "handle_message"
   # MUST accept arguments structured as shown here
   # MUST return :ok
   # Can do anything else within the function with the incoming message
   def handle_message(%{key: key, value: value} = message) do
-    IO.puts("Handling new kafka message")
-
-    Jason.decode(value)
+    body = Jason.decode(value)
       |>get_body
-      |>IO.inspect
+    #Logger.debug(inspect(body))
+    if body["event_type"] === "analytics.view"
+    do
+      body
       |>get_in(["view"])
       |>get_in(["pageUuid"])
-      |>IO.inspect
       |>store_uuid
+      |>SturdyUmbrellaWeb.ClockLive.count_changed
+    end
 
     :ok
   end
@@ -27,7 +30,6 @@ defmodule SturdyUmbrellaWeb.KafkaConsumer do
 
   def store_uuid(pageUuid) do
     :ets.update_counter(@name, pageUuid, {2, 1}, {pageUuid, 0})
-    |>IO.inspect
   end
 
 end
